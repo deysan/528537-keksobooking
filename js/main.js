27b13c09cf25b7b444995afc0083c41302bd000a
deysan <ideysan@gmail.com>
2018-12-08 20:40:55 +0200

    Обновил вывод информации обьявления

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ var OFFERS_NUMBER = 8;
 
 // Массивы
 var TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
-var TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var TYPES = [{type:'palace',name:'Дворец'}, {type:'flat',name:'Квартира'}, {type:'house',name:'Дом'}, {type:'bungalo',name:'Бунгало'}];
 var TIMES = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
@@ -58,6 +58,9 @@ var generateOffers = function () {
   var offers = [];
   for (var i = 0; i < OFFERS_NUMBER; i++) {
 
+    var locationX = getRandomNumber(LOCATION_MIN_Y, LOCATION_MAX_Y);
+    var locationY = getRandomNumber(LOCATION_MIN_Y, LOCATION_MAX_Y);
+
     offers[i] = {
       author: {
         avatar: 'img/avatars/user0' + getRandomNumber(1, 8) + '.png'
@@ -65,7 +68,7 @@ var generateOffers = function () {
 
       offer: {
         title: getRandomList(TITLES),
-        address: 'location.' + getRandomNumber(100, 1000) + ',' + 'location.' + getRandomNumber(100, 1000), // Временное решение
+        address: locationX + ', ' + locationY,
         price: getRandomNumberRound(PRICE_MIN, PRICE_MAX),
         type: getRandomList(TYPES),
         rooms: getRandomNumber(ROOMS_MIN, ROOMS_MAX),
@@ -73,13 +76,13 @@ var generateOffers = function () {
         checkin: getRandomList(TIMES),
         checkout: getRandomList(TIMES),
         features: getRandomSliceList(FEATURES),
-        description: '',
+        description: 'Описание',
         photos: getShuffleList(PHOTOS)
       },
 
       location: {
-        locationX: getRandomNumber(LOCATION_MIN_Y, LOCATION_MAX_Y),
-        locationY: getRandomNumber(LOCATION_MIN_Y, LOCATION_MAX_Y)
+        x: locationX,
+        y: locationY
       }
     };
   }
@@ -95,8 +98,8 @@ var openMap = function () {
 var generateOffersElement = function (offer) {
   var pin = pinTemplate.cloneNode(true);
   var avatar = pin.querySelector('img');
-  pin.style.left = offer.location.locationX + 'px';
-  pin.style.top = offer.location.locationY + 'px';
+  pin.style.left = offer.location.x + 'px';
+  pin.style.top = offer.location.y + 'px';
   avatar.src = offer.author.avatar;
   avatar.alt = offer.offer.title;
 
@@ -117,13 +120,13 @@ var generateCard = function (offer) {
   var card = cardTemplate.cloneNode(true);
   card.querySelector('.popup__title').textContent = offer.offer.title;
   card.querySelector('.popup__text--address').textContent = offer.offer.address;
-  card.querySelector('.popup__text--price').textContent = offer.offer.price + '₽/ночь';
-  card.querySelector('.popup__type').textContent = offer.offer.type;
+  card.querySelector('.popup__text--price').textContent = offer.offer.price + ' ₽/ночь';
+  card.querySelector('.popup__type').textContent = offer.offer.type.name;
   card.querySelector('.popup__text--capacity').textContent = offer.offer.rooms + ' комнаты для ' + offer.offer.guests + ' гостей';
   card.querySelector('.popup__text--time').textContent = 'Заезд после ' + offer.offer.checkin + ', выезд до ' + offer.offer.checkout;
   card.querySelector('.popup__features').textContent = offer.offer.features;
   card.querySelector('.popup__description').textContent = offer.offer.description;
-  card.querySelector('.popup__photos').src = offer.offer.photos;
+  card.querySelector('.popup__photo').src = offer.offer.photos;
   card.querySelector('.popup__avatar').src = offer.author.avatar;
 
   return card;
