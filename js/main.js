db980a7bc49676e60ce2c0a1dcd6c4149811e4f9
deysan <ideysan@gmail.com>
2018-12-26 01:14:47 +0200

    Добавил код перемещения курсора из учебного проекта

--- js/main.js
+++ js/main.js
@@ -256,3 +256,53 @@ var roomNumberAndCapacityChange = function () {
 };
 
 roomNumberElement.addEventListener('change', roomNumberAndCapacityChange);
+
+// Перемещения главного маркера
+mapPinElement.addEventListener('mousedown', function activateMap(evt) {
+  evt.preventDefault();
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var dragged = false;
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+    dragged = true;
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    mapPinElement.style.top = (mapPinElement.offsetTop - shift.y) + 'px';
+    mapPinElement.style.left = (mapPinElement.offsetLeft - shift.x) + 'px';
+
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+
+    if (dragged) {
+      var onClickPreventDefault = function (removeEvt) {
+        removeEvt.preventDefault();
+        mapPinElement.removeEventListener('click', onClickPreventDefault)
+      };
+      mapPinElement.addEventListener('click', onClickPreventDefault);
+    }
+
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
