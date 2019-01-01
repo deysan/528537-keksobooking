113cc2a1b4db3e9a8e983ffd9b9a71e9716ccacb
deysan <ideysan@gmail.com>
2019-01-01 14:34:19 +0200

    Добавил код перемещения курсора

--- js/main.js
+++ js/main.js
@@ -287,3 +287,40 @@ timesInElement.addEventListener('change', function (evt) {
 timesOutElement.addEventListener('change', function (evt) {
   timesInElement.value = evt.target.value;
 });
+
+// Перемещения главного маркера
+mapPinElement.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    mapPinElement.style.top = (mapPinElement.offsetTop - shift.y) + 'px';
+    mapPinElement.style.left = (mapPinElement.offsetLeft - shift.x) + 'px';
+
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
