8b740aabb9092fa736740e32ea19908386cc69ea
deysan <ideysan@gmail.com>
2019-01-10 14:36:43 +0200

    Добавил фильтр по цене

--- js/filter.js
+++ js/filter.js
@@ -4,10 +4,22 @@
 
   var mapFiltersElement = document.querySelector('.map__filters');
   var filterTypeElement = document.querySelector('#housing-type');
+  var filterPriceElement = document.querySelector('#housing-price');
 
   var filteredType = function (offer) {
     return (filterTypeElement.value === 'any' || filterTypeElement.value === offer.offer.type);
   };
+
+  var filteredPrice = function (offer) {
+    if (filterPriceElement.value === 'low') {
+      return (10000 > offer.offer.price);
+    } else if (filterPriceElement.value === 'middle') {
+      return (10000 <= offer.offer.price && offer.offer.price < 50000);
+    } else if (filterPriceElement.value === 'high') {
+      return (50000 <= offer.offer.price);
+    }
+    return true;
+};
 
   var onFilterChange = function (func) {
     mapFiltersElement.addEventListener('change', func);
@@ -17,7 +29,7 @@
     window.card.remove();
     window.pin.remove();
     var filterOffer = array.filter(function (offer) {
-      return (filteredType(offer));
+      return (filteredType(offer) && filteredPrice(offer));
     });
     return filterOffer;
   };
